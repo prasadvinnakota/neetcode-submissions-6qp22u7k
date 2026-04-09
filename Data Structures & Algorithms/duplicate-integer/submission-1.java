ca9ccbf8c57f316f2002cbd2a64686e4abb51d04
prasadvinnakota <68739045+prasadvinnakota@users.noreply.github.com>
2026-04-09 08:01:45 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+     HashSet<Integer> numContains = new HashSet<>();
+
+     for(int i : nums)
+     {
+        if(!numContains.add(i))
+        {
+            return true;
+        }
+     }
+
+     return false;
+
+
+    }
+}
